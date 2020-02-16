ee1a44c187cebe241d14b85af4a3e253f5f53a3c
ruslanlevv <ruslanleev@gmail.com>
2020-02-16 16:10:52 +0300

    Updates

---
+++ precache-manifest.9ee82b057056f11a82bb94ba208b5280.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec44a9e59ddd9a8b5c8dfbf165cc704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "418d97123c84396d9051",
+    "url": "/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "61b6b21ae6e117569525",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "418d97123c84396d9051",
+    "url": "/static/js/2.ad7a5439.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.ad7a5439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b6b21ae6e117569525",
+    "url": "/static/js/main.4c6088a5.chunk.js"
+  },
+  {
+    "revision": "609bedb9c09f97be70eb",
+    "url": "/static/js/runtime-main.7a5752ac.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
